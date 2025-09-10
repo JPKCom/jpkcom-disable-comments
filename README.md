dd091adc5cc2554a250d89c23fdef7980de7dd04
Jean Pierre Kolb <jp.kolb@creationell.de>
2025-09-10 14:11:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# JPKCom Disable Comments
+
+**Plugin Name:** JPKCom Disable Comments  
+**Plugin URI:** https://github.com/JPKCom/jpkcom-disable-comments  
+**Description:** Globally disable comments functionality.  
+**Version:** 1.0.0  
+**Author:** Jean Pierre Kolb <jpk@jpkc.com>  
+**Author URI:** https://www.jpkc.com  
+**Contributors:** JPKCom  
+**Tags:** Comments, Plugin  
+**Requires Plugins:** livecanvas  
+**Requires at least:** 6.7  
+**Tested up to:** 6.8  
+**Requires PHP:** 8.3  
+**Stable tag:** 1.0.0  
+**License:** GPL-2.0+  
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.txt
+
+Globally disable comments functionality.
+
+
+## Description
+
+Disables the WordPress core functionality for comments for the whole site.
+
+
+## Installation
+
+1. In your admin panel, go to 'Plugins' > and click the 'Add New' button.
+2. Click Upload Plugin and 'Choose File', then select the Plugin's .zip file. Click 'Install Now'.
+3. Click 'Activate' to use the plugin right away.
+
+
+## Changelog
+
+### 1.0.0
+* Initial Release
---
+++ jpkcom-disable-comments.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Plugin Name: JPKCom Disable Comments
+Plugin URI: https://github.com/JPKCom/jpkcom-disable-comments
+Description: Globally disable comments functionality.
+Version: 1.0.0
+Author: Jean Pierre Kolb <jpk@jpkc.com>
+Author URI: https://www.jpkc.com
+Contributors: JPKCom
+Tags: Comments, Plugin
+Requires at least: 6.7
+Tested up to: 6.8
+Requires PHP: 8.3
+Stable tag: 1.0.0
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+GitHub Plugin URI: JPKCom/jpkcom-disable-comments
+Primary Branch: main
+*/
+
+if ( ! defined( constant_name: 'WPINC' ) ) {
+  die;
+}
+
+add_action( 'admin_init', function (): void {
+    global $pagenow;
+
+    if ( $pagenow === 'edit-comments.php' ) {
+        wp_safe_redirect( admin_url() );
+        exit;
+    }
+
+    remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+
+    foreach ( get_post_types() as $post_type ) {
+        if ( post_type_supports( $post_type, 'comments' ) ) {
+            remove_post_type_support( $post_type, 'comments' );
+            remove_post_type_support( $post_type, 'trackbacks' );
+        }
+    }
+} );
+
+add_filter( 'comments_open', '__return_false', 20, 2 );
+add_filter( 'pings_open', '__return_false', 20, 2 );
+add_filter( 'comments_array', '__return_empty_array', 10, 2 );
+
+add_action( 'admin_menu', function (): void {
+    remove_menu_page( 'edit-comments.php' );
+} );
+
+add_action( 'admin_bar_menu', function ( WP_Admin_Bar $wp_admin_bar ): void {
+    $wp_admin_bar->remove_node( 'comments' );
+}, 999 );
